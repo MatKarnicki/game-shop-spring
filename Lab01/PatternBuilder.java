a29fe82999d38a5fa7f00f852d55df681efa4e08
Mateusz Karnicki <matkarnicki@gmail.com>
2024-10-16 09:28:23 +0200

    create Patterns overloaded

--- Lab01/PatternBuilder.java
+++ Lab01/PatternBuilder.java
@@ -1,6 +1,7 @@
     import java.util.Scanner;
-    public class Zad4 {
-   public static void main(String[] args){
+    public class PatternBuilder {
+        static String patternSymbol = "X";
+        public static void main(String[] args){        
         Scanner myScanner = new Scanner(System.in);
         int numberOfRows;
          do {
@@ -15,91 +16,54 @@
             }
         } while (numberOfRows <= 0);
         String expected = "X\nXX\nXX\nX\nXX\n X\n X\nXX\n";
-        String actual = createPatterns(numberOfRows);
+        String actual = createPatterns(numberOfRows, "X");
         System.out.println(expected.equals(actual));
         myScanner.close();
    }
 
-
     public static String createPatterns(int numberOfRows) {
         
         StringBuilder result = new StringBuilder();
-
+        
         for (int i = 1; i <= numberOfRows; i++) {
             for (int j = 1; j <= i; j++ ){
-                result.append("X");
+                result.append(patternSymbol);
             }
             result.append("\n");
         }
-
+        
         for (int i = numberOfRows; i > 0; i--) {
             for (int j = 1; j <= i; j++ ){
-                result.append("X");
+                result.append(patternSymbol);
             }
             result.append("\n");
         }
-
+        
         for (int i = 1; i <= numberOfRows; i++) {
             for (int j = 1; j < i; j++){
-                 result.append(" ");
+                result.append(" ");
             }
             for (int j = 1; j <= numberOfRows - i + 1; j++ ){
-                result.append("X");
+                result.append(patternSymbol);
             }
-                result.append("\n");
+            result.append("\n");
         }
-
+        
         for (int i = numberOfRows; i > 0; i--) {
-             for (int j = 1; j < i; j++){
-                 result.append(" ");
+            for (int j = 1; j < i; j++){
+                result.append(" ");
             }
             for (int j = 1; j <= numberOfRows - i + 1; j++ ){
-                result.append("X");
+                result.append(patternSymbol);
             }
-                result.append("\n");
+            result.append("\n");
         }
         return result.toString();
-
+        
     }
-    
-    public static String createPatterns(int numberOfRows, String patternSymbol) {
-        
-        StringBuilder result = new StringBuilder();
-        
-        for (int i = 1; i <= numberOfRows; i++) {
-            for (int j = 1; j <= i; j++ ){
-                result.append(patternSymbol);
-            }
-            result.append("\n");
-        }
-        
-        for (int i = numberOfRows; i > 0; i--) {
-            for (int j = 1; j <= i; j++ ){
-                result.append(patternSymbol);
-            }
-            result.append("\n");
-        }
-        
-        for (int i = 1; i <= numberOfRows; i++) {
-            for (int j = 1; j < i; j++){
-                result.append(" ");
-            }
-            for (int j = 1; j <= numberOfRows - i + 1; j++ ){
-                result.append(patternSymbol);
-            }
-            result.append("\n");
-        }
-        
-        for (int i = numberOfRows; i > 0; i--) {
-            for (int j = 1; j < i; j++){
-                result.append(" ");
-            }
-            for (int j = 1; j <= numberOfRows - i + 1; j++ ){
-                result.append(patternSymbol);
-            }
-            result.append("\n");
-        }
-        return result.toString();
-        
+
+    public static String createPatterns(int numberOfRows, String Character){
+        patternSymbol = Character;
+        return createPatterns(numberOfRows);
     }
 }
