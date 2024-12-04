e6bb00679877a87ffcf5b764bac60344a3e74463
Mateusz Karnicki <matkarnicki@gmail.com>
2024-12-04 09:03:03 +0100

    lab06

---
+++ Lab06/game.rest/src/main/java/ug/edu/game/rest/GameApplication.java
@@ -0,0 +1,18 @@
+package ug.edu.game.rest;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ug.edu.game.rest.service.GameService;
+@SpringBootApplication
+public class GameApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initialize(GameService gameService) {
+		return args -> gameService.initializeDatabase();
+	}
+}
---
+++ Lab06/game.rest/src/main/java/ug/edu/game/rest/domain/GameDetails.java
@@ -0,0 +1,65 @@
+package ug.edu.game.rest.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class GameDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Developer name cannot be empty.")
+    private String developer;
+
+    @NotBlank(message = "Publisher name cannot be empty.")
+    private String publisher;
+
+    private String description;
+
+    @OneToOne
+    private Game game;
+
+    public GameDetails() {}
+
+    public GameDetails(String developer, String publisher, String description) {
+        this.developer = developer;
+        this.publisher = publisher;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "GameDetails [id=" + id + ", developer=" + developer +
+                ", publisher=" + publisher + ", description=" + description + "]";
+    }
+}
